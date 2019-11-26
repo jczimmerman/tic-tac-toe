88bb808c01f85005182845c85a25a6d8bada54b3
Joey Zimmerman <joeyzimmerman17@gmail.com>
2019-11-26 13:00:43 -0500

    Add function for switching, change start function

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 let grid = [
-1, 2, 3,
-4, 5, 6,
-7, 8, 9];
+"", "", "",
+"", "", "",
+"", "", ""];
 
 const players = ['X', 'O'];
+
 let turn;
 
 //Function to choose who starts randomly
 let randomStart = () =>{
   let randy = Math.random() * 100;
 
-  if (randy < 50)  {
-    turn = 0;
-  }
-  else {
-    turn = 1;
-  }
+  randy < 50 ? turn = 0 : turn = 1;
 }
 
-randomStart();
 //Display whose turn it is
 let  text = document.getElementById("text");
-text.textContent = "It's " + players[turn] + "'s turn";
+let updateTurn = () =>{
+  text.textContent = "It's " + players[turn] + "'s turn";
+}
 
 //Loops through all divs, gives them an event
 let allDivs = document.querySelectorAll("div");
+
 for (let element of allDivs){
-  
   element.addEventListener("click", event =>{
     element.textContent = players[turn];
     turn == 0 ? turn = 1 : turn = 0;
+    updateTurn();
   });
 }
+
+//These will be in start/restart function
+randomStart();
+updateTurn();
