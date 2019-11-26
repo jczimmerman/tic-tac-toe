7cf634a564f354540067f41d914d024ea57939dc
Nick Card <nickcard2000@gmail.com>
2019-11-26 15:49:33 -0500

    Nothing really worked on the tie condition then worked with joey to get it done

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let grid = [
 const players = ['X', 'O'];
 
 let turn;
-
+let counter = 0;
 //Function to choose who starts randomly
 let randomStart = () =>{
   let randy = Math.random() * 100;
@@ -29,8 +29,9 @@ for (let element of allDivs){
     element.textContent = players[turn];
     grid[element.id] = players[turn];
     turn == 0 ? turn = 1 : turn = 0;
+    counter += 1;
+    console.log(counter);
     element.removeEventListener("click", eventUpdate);
-
     checkWin();
     updateTurn();
   }
@@ -42,23 +43,33 @@ for (let element of allDivs){
 randomStart();
 updateTurn();
 
-let checkWin = () =>{
+let checkWin = () => {
+  
   if (grid[0] != "") {
+
+    console.log('check', '1');
     if ((grid[0] == grid[1] && (grid[0] == grid[2]) || grid[0] == grid[3] && grid[0] == grid[6])
     || grid[0] == grid[4] && grid[0] == grid[8]){
-      console.log("winner!");
+      return true;
+      
     }
   }
-  if (grid[4] != ""){
+  if (grid[4] != "") {
+    console.log('check', '2');
     if((grid[4] == grid[3] && grid[4] == grid[5]) || (grid[4] == grid[7] && grid[4] == grid[1])
     || (grid[4] == grid[6] && grid[4] == grid[2])){
-      console.log("winner!")
+      return true;
+      
     }
   }
-  if (grid[8] != ""){
+  if (grid[8] != "") {
+    console.log('check', '3');
     if((grid[8] == grid[7] && grid[8] == grid[6]) || (grid[8] == grid[5] && grid[8] == grid[2])){
-      console.log("winner!")
+      return true;
     }
+  }
+  if (checkWin() == false) {
+    console.log("tie");
   }
 }
 
